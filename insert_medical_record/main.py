fb693b968476500b3c482861244afeeb2c73cb5b
Shyam Kachhadiya <shyam.k@smytten.com>
2024-02-05 07:30:09 +0530

    sql achemy crud example.

--- insert_medical_record/main.py
+++ insert_medical_record/main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 import flask
-import psycopg2
-from psycopg2 import sql
-import os
-from psycopg2.extras import RealDictCursor
 from types import SimpleNamespace
 import json
+from sqlalchemy import Column, DateTime, Integer, String, create_engine, Date
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from datetime import datetime
+from api_response import APIResponse
 
 db_params = {
     'host': os.environ.get('DB_HOST'),
@@ -15,135 +16,63 @@ db_params = {
     'database': os.environ.get('DB_NAME'),
 }
 
-def execute_query(connection, query, params=None):
-    with connection.cursor(cursor_factory=RealDictCursor) as cursor:
-        if params:
-            cursor.execute(query, params)
-        else:
-            cursor.execute(query)
-        result = cursor.fetchall()
-        return result
+print(db_params)
+# Construct the database URL
+db_url = f"postgresql://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['database']}"
 
-def execute_insert_query(data):
-    # Connect to the PostgreSQL database
-    if data is None or not data:
-        return 1 
-    resp = 1
-    with psycopg2.connect(**db_params) as conn:
-        with conn.cursor() as cursor:
-            try:
-                # Start a transaction
-                conn.autocommit = False
+# Create the database engine
+engine = create_engine(db_url, echo=True)
 
-                # Insert data into the "main_table"
-                sql_main = """
-                    INSERT INTO mo_records (opd_date, opd_type, updated)
-                    VALUES (%s, %s, CURRENT_TIMESTAMP)
-                    RETURNING id;
-                """
+# Create a session factory
+Session = sessionmaker(bind=engine)
 
-                cursor.execute(sql_main, (data.opd_date, data.opd_type))
-                record_id = cursor.fetchone()[0]
-                
+# Create a session
+session = Session()
+Base = declarative_base()
 
-                print(record_id)
-                # Insert data into the "groups_table" using the generated id
-                sql_groups = "INSERT INTO record_groups (name, male, female, record_id) VALUES (%s, %s, %s, %s)"
-                for group in data.groups:
-                    cursor.execute(sql_groups, (group.name, group.male, group.female, record_id))
+class Record(Base):
+    __tablename__ = 'mo_records'
+    id = Column(Integer, primary_key=True)
+    opd_type = Column(Integer, name="opd_type")
+    updated_at = Column(DateTime, default=datetime.utcnow, name="updated_at")
+    opd_date = Column(Date, name="opd_date")
 
-                # Commit the transaction
-                conn.commit()
-                print("Data inserted successfully.")
+class RecordGroup(Base):
+    __tablename__ = 'record_groups'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, name="name")
+    new_male = Column(Integer, name="new_male")
+    new_female = Column(Integer, name="new_female")
+    old_male = Column(Integer, name="old_male")
+    old_female = Column(Integer, name="old_female")
+    record_id = Column(Integer, name="record_id")
 
-            except Exception as e:
-                # Rollback the transaction if an error occurs
-                conn.rollback()
-                print(f"Error: {e}")
-                resp = 1
-
-            finally:
-                # Restore autocommit to its default state
-                conn.autocommit = True
-    resp = 0
-    return resp
-
-
-def execute_update_query(data):
-    if data is None or not data:
-        return 1 
-    resp = 1
-    with psycopg2.connect(**db_params) as conn:
-        with conn.cursor() as cursor:
-            try:
-                # Start a transaction
-                conn.autocommit = False
-
-                # Insert data into the "main_table"
-                sql_main = """
-                    UPDATE mo_records set opd_date = %(opd_date)s, opd_type=%(opd_type)s, updated=CURRENT_TIMESTAMP
-                    where id = %(id)s
-                """
-                sql_body = {
-                    'opd_date': data.opd_date,
-                    'opd_type': data.opd_type,
-                    'id': data.id
-                }
-                cursor.execute(sql_main, sql_body)
-
-                # Insert data into the "groups_table" using the generated id
-                sql_groups = """
-                     UPDATE record_groups
-                        SET male = %(male)s, female = %(female)s
-                        WHERE id = %(id)s
-                """
-                for group in data.groups:
-                    group_data = {
-                        'name': group.name,
-                        'male': group.male,
-                        'female': group.female,
-                        'id': group.id,
-                    }
-                    cursor.execute(sql_groups, group_data)
-                # Commit the transaction
-                conn.commit()
-                print("Data updated successfully.")
-
-            except Exception as e:
-                # Rollback the transaction if an error occurs
-                conn.rollback()
-                print(f"Error: {e}")
-                resp = 1
-
-            finally:
-                # Restore autocommit to its default state
-                conn.autocommit = True
-    resp = 0
-    return resp
-
-    
 
 def insert_medical_record(request: flask.Request) -> flask.typing.ResponseReturnValue:
     # Use request.get_json() to get parsed JSON data
     json_data = request.get_json()
 
     # Convert the parsed JSON data to SimpleNamespace
-    data = json.loads(json.dumps(json_data), object_hook=lambda d: SimpleNamespace(**d))
+    #data = json.loads(json.dumps(json_data), object_hook=lambda d: SimpleNamespace(**d))
     
-    # Access attributes and print
-    print(data)
-    if hasattr(data, 'groups') and len(data.groups) > 0:
-        record_id = getattr(data, 'id', None)
-        if record_id == None:
-            print("id is none")
-            resp = execute_insert_query(data=data)
-        else:
-            print("id is not none")
-            resp = execute_update_query(data=data)
-    else:    
-        print("No groups present.")
-        return "something bad", 500
-    # Return a response (optional)
-         # Convert SimpleNamespace to dictionary and return as JSON response
-    
-    return "something : " + str(resp), 200
+    parsed_opd_date = datetime.strptime(json_data["opd_date"], "%a, %d %b %Y %H:%M:%S %Z").date()
+    parsed_updated_at = datetime.strptime(json_data["updated_at"], "%a, %d %b %Y %H:%M:%S %Z")
+    record_saved = Record(
+        id=json_data.get("id"),
+        opd_type=json_data["opd_type"],
+        opd_date=parsed_opd_date,
+        updated_at=parsed_updated_at
+    )
+
+    record_saved = session.merge(record_saved)
+    session.query(RecordGroup).filter(RecordGroup.record_id == record_saved.id).delete()
+    new_group_data = [
+        RecordGroup(name='0-15 years', new_male=10, new_female=20, old_male=5, old_female=15, record_id=record_saved.id),
+        RecordGroup(name='15-60 years', new_male=15, new_female=25, old_male=8, old_female=18, record_id=record_saved.id),
+        RecordGroup(name='60+ years', new_male=15, new_female=25, old_male=8, old_female=18, record_id=record_saved.id),
+    ]
+    session.add_all(new_group_data)
+    session.commit()
+
+    return APIResponse.ok_with_data("data saved successfully")
+
